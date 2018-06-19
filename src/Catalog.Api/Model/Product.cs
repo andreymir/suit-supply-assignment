54a20697a0f8aab74999b01be7c3a960a90cd181
Andrey Mironov <andrey.mir@gmail.com>
2018-06-19 20:39:14 +0300

    Init Catalog.Api project

---
+++ src/Catalog.Api/Model/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Catalog.Api.Model
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string PhotoUrl { get; set; }
+        public decimal Price { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
